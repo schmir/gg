14f42f0cbd2d49727f09231c56423c6e7ab6ebab
Ralf Schmitt <ralf@systemexit.de>
2011-03-09 14:36:39 +0100

    test_gg_basic.py: test get_reachable_from

--- test_gg_basic.py
+++ test_gg_basic.py
@@ -29,13 +29,34 @@ def test_intvector_append():
 
     assert list(g) == range(1000)
 
+
 def test_intvector_empty_init():
     g = _gg.intvector()
     assert list(g) == []
+
 
 def test_intvector_init_from_list():
     g = _gg.intvector(range(100))
     assert list(g) == range(100)
 
+
 def test_intvector_init_from_list_with_float():
     py.test.raises(TypeError, "_gg.intvector([0, 1.5])")
+
+
+def test_get_reachable_from_empty_graph():
+    g = _gg.graph()
+    res = g.get_reachable_from(_gg.intvector(range(10)))
+    assert list(res) == range(10)
+
+
+def test_get_reachable_from():
+    g = _gg.graph()
+    g.add_link(1, 2)
+    g.add_link(1, 3)
+    g.add_link(2, 4)
+    res = g.get_reachable_from(_gg.intvector([1]))
+    assert set(res) == set([1, 2, 3, 4])
+
+    res = g.get_reachable_from(_gg.intvector([4]))
+    assert set(res) == set([4])
